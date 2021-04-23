334210a6cfd52e788862b526e43b39ae018d12b1
wisdom <wisdomwashe500@gmail.com>
2021-04-23 22:52:08 +0300

    initial commit

--- DB.py
+++ DB.py
@@ -6,17 +6,44 @@ def createdb():
     # Create a cursor
     c = conn.cursor()
     # Create a Table Datatype text
-    c.execute("CREATE TABLE data (username text, password text)")
+    c.execute("CREATE TABLE data (name text, username text, password text)")
     # Commit our command
     conn.commit()
 
-def insertdb(username, password):
+def insertdb(name, username, password):
     # Creating and connecting to the Database
     conn = sqlite3.connect('data.db')
     # Create a cursor
     c = conn.cursor()
     # Inserting Data
-    c.execute("INSERT INTO data (username, password) VALUES (?,?)",(username, password))
+    c.execute("INSERT INTO data (name, username, password) VALUES (?,?,?)",(name, username, password))
+    # Commit our command
+    conn.commit()
+
+def fetchall():
+    # Creating and connecting to the Database
+    conn = sqlite3.connect('data.db')
+    # Create a cursor
+    c = conn.cursor()
+    # Getting formated contents
+    c.execute("SELECT rowid, * FROM data")
+
+    info = c.fetchall()
+    print("ID" +"\tUsername" +"\tPassword" +"\tName")
+    print("--" +"\t--------" +"\t--------"+"\t----")
+    for i in info:
+        print (str(i[0]) +"\t\t" + i[1]+"\t\t" + i[2]+"\t\t" + i[3])
+
+    # Commit our command
+    conn.commit()
+
+def delete(id):
+    # Creating and connecting to the Database
+    conn = sqlite3.connect('data.db')
+    # Create a cursor
+    c = conn.cursor()
+    # Inserting Data
+    c.execute("DELETE from data WHERE rowid = {}".format(id))
     # Commit our command
     conn.commit()
 
--- main.py
+++ main.py
@@ -1,6 +1,50 @@
 import DB as d
 
-username = "x"
-password = "w"
 
-d.insertdb(username, password)
+def data():
+    # d.createdb()
+    name = input("Enter a name: ")
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
+    d.insertdb(name, username, password)
+
+def create():
+    d.createdb()
+
+def delete():
+    id = int(input("Enter id: "))
+    d.delete(id)
+
+def main():
+    com = input("Get help (h) or enter a command: ").lower()
+
+    if com == "h":
+        print("""    ##Commmands##
+    >Create a database(c)
+    >Delete a row(d)
+    >Show data(s)
+    >Add data(a)
+    >Quit(q)
+        """)
+        main()
+    elif com == "c":
+        d.createdb()
+        main()
+    elif com == "d":
+        id = input("Enter ID: ")
+        d.delete(id)
+        main()
+    elif com == "s":
+        d.fetchall()
+        main()
+    elif com == "a":
+        data()
+        main()
+    elif com == "q":
+        quit()
+    else:
+        print("Invalid option")
+        main()
+
+if __name__ == '__main__':
+    main()
